cbfc80c2e424257d5656f75626bd45e3fa75b84b
sumin <suminkim.me@gmail.com>
2023-12-21 12:48:23 +0900

    fix: ep05-ESM 버전으로 변경하고 타입 정의해서 배포

--- ep05/src/index.cjs
+++ ep05/src/index.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  ver: "cjs",
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
---
+++ ep05/src/index.esm.js
@@ -0,0 +1,4 @@
+export const ver = "esm";
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
